5c7cf0f586bbf5f65be3b9b9aa76e735b4778e6f
Lucas Pedrosa <54561127+pedrosalucas@users.noreply.github.com>
2022-03-30 13:02:19 -0300

    Getting photo data using redux context

--- src/Components/Photo/Photo.js
+++ src/Components/Photo/Photo.js
@@ -1,35 +1,35 @@
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import { PHOTO_GET } from '../../api';
-import useFetch from '../../Hooks/useFetch';
-import Error from '../Helper/Error';
-import Head from '../Helper/Head';
-import Loading from '../Helper/Loading';
-import PhotoContent from './PhotoContent';
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+import { getPhoto } from "../../Store/photo";
+import Error from "../Helper/Error";
+import Head from "../Helper/Head";
+import Loading from "../Helper/Loading";
+import PhotoContent from "./PhotoContent";
 
 const Photo = () => {
-    const { id } = useParams();
-    const { data, error, loading, request } = useFetch();
+  const { id } = useParams();
+  const { data, error, loading } = useSelector((state) => state.photo);
+  const dispatch = useDispatch();
 
-    React.useEffect(() => {
-        const { url, options } = PHOTO_GET(id);
-        request(url, options);
-    }, [request, id]);
+  React.useEffect(() => {
+    dispatch(getPhoto(id));
+  }, [dispatch, id]);
 
-    if( error ) {
-        return <Error error={error} />;
-    } else if( loading ) {
-        return <Loading />;
-    } else if( data ) {
-        return (
-            <section className="container mainContainer">
-                <Head title={data.photo.title} />
-                <PhotoContent data={data} single={true} />
-            </section>
-        );
-    } else {
-        return null;
-    }
+  if (error) {
+    return <Error error={error} />;
+  } else if (loading) {
+    return <Loading />;
+  } else if (data) {
+    return (
+      <section className="container mainContainer">
+        <Head title={data.photo.title} />
+        <PhotoContent data={data} single={true} />
+      </section>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Photo;
---
+++ src/Store/photo.js
@@ -0,0 +1,46 @@
+import { PHOTO_GET } from "../api";
+
+const { createSlice } = require("@reduxjs/toolkit");
+
+const slice = createSlice({
+  name: "photo",
+  initialState: {
+    data: null,
+    loading: false,
+    error: null,
+  },
+  reducers: {
+    fetchStarted(state) {
+      state.loading = true;
+      state.error = null;
+    },
+    fetchSuccess(state, action) {
+      state.data = action.payload;
+      state.loading = false;
+    },
+    fetchError(state, action) {
+      state.loading = false;
+      state.error = action.payload;
+    },
+  },
+});
+
+const { fetchStarted, fetchSuccess, fetchError } = slice.actions;
+
+export const getPhoto = (id) => async (dispatch) => {
+  try {
+    dispatch(fetchStarted());
+
+    const { url, options } = PHOTO_GET(id);
+    const response = await fetch(url, options);
+    const data = await response.json();
+
+    if (response.ok === false) throw new Error(data.message);
+
+    dispatch(fetchSuccess(data));
+  } catch (error) {
+    dispatch(fetchError(error.message));
+  }
+};
+
+export default slice.reducer;
